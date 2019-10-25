8f65fe5e029bca67d431687717595538cfbb37a8
chenxz <563859095@qq.com>
2019-10-25 21:08:26 +0800

    Initial commit

---
+++ app/src/main/java/com/cxz/kotlin/samples/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cxz.kotlin.samples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
